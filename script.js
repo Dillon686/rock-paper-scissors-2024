533368331e6dc16a5940f8b1f500bb1f8630c907
Dillon Richardson <dillonrichardson6@gmail.com>
2024-04-03 11:38:23 -0400

    Added gameplay loop and tested successfully

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let totalWins = 0;
+
 function getComputerChoice(){
     let result = Math.floor(Math.random() * 3);
     
@@ -22,14 +24,32 @@ function playRound(playerSelection, computerSelection){
     }else if (playerSelection === "rock" && computerSelection === "paper"){
         return "You lose! Rock lost to paper."
     }else if (playerSelection === "rock" && computerSelection === "scissors"){
+        totalWins++;
         return "You win! Rock beat scissors."
     }else if (playerSelection === "paper" && computerSelection === "rock"){
+        totalWins++;
         return "You win! Paper beat rock."
     }else if (playerSelection === "paper" && computerSelection === "scissors"){
         return "You lose! Paper lost to scissors."
     }else if (playerSelection === "scissors" && computerSelection === "rock"){
         return "You lose! Scissors lost to rock."
     }else if (playerSelection === "scissors" && computerSelection === "paper"){
+        totalWins++;
         return "You win! Scissors beat paper."
     }
 }
+
+function playGame(){
+    for (let i=0; i < 5; i++){
+        const choice = prompt('Please type "rock", "paper", or "scissors" to begin the game.');
+        console.log(playRound(choice, getComputerChoice()));
+    }
+
+    if (totalWins > 2){
+        console.log(`You won the majority of the games! ${totalWins} wins!`)
+    }else{
+        console.log("You didn't win the majority of rounds :(");
+    }
+}
+
+playGame();
