dd4b8444c68c1dd668adb6ea1f731be8c891535a
Dillon Richardson <dillonrichardson6@gmail.com>
2024-04-03 10:55:34 -0400

    Adding function to play a round

--- script.js
+++ script.js
@@ -3,13 +3,33 @@ function getComputerChoice(){
     
     switch(result){
         case 0:
-            return "Rock";
+            return "rock";
             break;
         case 1:
-            return "Paper";
+            return "paper";
             break;
         case 2:
-            return "Scissors";
+            return "scissors";
             break;
+    }
+}
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection){
+        return `It was a tie! You both chose ${computerSelection}`
+    }else if (playerSelection === "rock" && computerSelection === "paper"){
+        return "You lose! Rock lost to paper."
+    }else if (playerSelection === "rock" && computerSelection === "scissors"){
+        return "You win! Rock beat scissors."
+    }else if (playerSelection === "paper" && computerSelection === "rock"){
+        return "You win! Paper beat rock."
+    }else if (playerSelection === "paper" && computerSelection === "scissors"){
+        return "You lose! Paper lost to scissors."
+    }else if (playerSelection === "scissors" && computerSelection === "rock"){
+        return "You lose! Scissors lost to rock."
+    }else if (playerSelection === "scissors" && computerSelection === "paper"){
+        return "You win! Scissors beat paper."
     }
 }
